4e2540986917ce472fe6cd72a7f41a92690f1ed6
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-01 23:25:49 -0600

    Preserve player names in movie playback

--- go_client/movie_test.go
+++ go_client/movie_test.go
@@ -2,14 +2,16 @@ package main
 
 import "testing"
 
-func TestParseMovieRegistersPlayers(t *testing.T) {
+func TestParseMovie(t *testing.T) {
+	debug = false
 	players = make(map[string]*Player)
 	state.descriptors = make(map[uint8]frameDescriptor)
 	state.mobiles = make(map[uint8]frameMobile)
-	if _, err := parseMovie("test.clMov"); err != nil {
+	frames, err := parseMovie("test.clMov")
+	if err != nil {
 		t.Fatalf("parseMovie: %v", err)
 	}
-	if len(players) == 0 {
-		t.Fatalf("players not registered")
+	if len(frames) == 0 {
+		t.Fatalf("no frames parsed")
 	}
 }
